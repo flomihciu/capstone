2a051430ab28314b06529a48b59f9592818bb258
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-04-19 13:56:06 -0500

    EKS updated

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -12,3 +12,13 @@ output "eks_cluster_region" {
   description = "AWS region for the EKS cluster"
   value       = var.aws_region
 }
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint URL"
+  value       = aws_eks_cluster.team_rocket_cluster.endpoint
+}
+
+output "eks_cluster_ca" {
+  description = "Base64-encoded certificate authority data for EKS cluster"
+  value       = aws_eks_cluster.team_rocket_cluster.certificate_authority[0].data
+}

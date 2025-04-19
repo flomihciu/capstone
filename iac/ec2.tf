4db949638b0bc923da08b8b3531da89b0862d127
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-04-19 09:30:16 -0500

    Adding updates

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "team_rocket_dev" {
-  ami                         = "ami-084568db4383264d4" #ubuntu
+  ami                         = "ami-04f167a56786e4b09" #ubuntu
   instance_type               = var.ec2_instance_type
   subnet_id                   = aws_subnet.public_subnet_1.id
   vpc_security_group_ids      = [aws_security_group.eks_node_sg.id]

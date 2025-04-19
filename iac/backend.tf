0fb66a885ae6f13e9599fb6fc938bd33a5789cb2
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-04-19 09:37:02 -0500

    Adding updates

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "team-rocket-terraform-state"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "team-rocket-lock-table"
   }
 }

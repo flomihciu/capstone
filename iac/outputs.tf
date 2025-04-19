df73f2e4ee0e014f4dc913f1548efc372f030819
Florin Mihai Ciuperciuc <flomihciu@gmail.com>
2025-04-19 13:35:21 -0500

    EKS updated

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -7,3 +7,8 @@ output "eks_cluster_name" {
   description = "EKS cluster name for reference"
   value       = aws_eks_cluster.team_rocket_cluster.name
 }
+
+output "eks_cluster_region" {
+  description = "AWS region for the EKS cluster"
+  value       = var.aws_region
+}
